7cf37d983e814f768b2fe10081b0fb96f01acce7
CunYe <784396136@qq.com>
2018-09-04 18:04:17 +0800

    添加搜索功能

--- models/Blog.php
+++ models/Blog.php
@@ -7,7 +7,35 @@ class Blog extends Base
 
     public function search()
     {
-        $stmt = self::$pdo->query('SELECT * FROM blogs');
+        $where = 1;
+        $value = [];
+        // 如果有keyword值并且不为空时添加where条件
+        if(isset($_GET['keywords']) && $_GET['keywords'])
+        {
+            $where .= " AND (title like '%{$_GET['keywords']}%' OR content like '%{$_GET['keywords']}%')";
+            $value[] = $_GET['keywords'];
+        }
+        // 如果有start_date值并且不为空时添加where条件
+        if(isset($_GET['start_date']) && $_GET['start_date'])
+        {
+            $where .= " AND created_at >= '{$_GET['start_date']}'";
+            $value[] = $_GET['start_date'];
+        }
+        // 如果有end_date值并且不为空时添加where条件
+        if(isset($_GET['end_date']) && $_GET['end_date'])
+        {
+            $where .= " AND created_at <= '{$_GET['end_date']}'";
+            $value[] = $_GET['end_date'];
+        }
+        // 如果有is_show值并且不为空时添加where条件
+        if(isset($_GET['is_show']) && $_GET['is_show'])
+        {
+            $where .= " AND is_show = {$_GET['is_show']}";
+            $value[] = $_GET['is_show'];
+        }
+        // 进行预处理
+        $stmt = self::$pdo->prepare("SELECT * FROM blogs WHERE $where");    
+        $stmt->execute($value);
         $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $data;
     }
